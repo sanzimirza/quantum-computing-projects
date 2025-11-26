48662af4401fec8ada55a41de515e98cf47cddb1
sanzimirza <156568336+sanzimirza@users.noreply.github.com>
2025-11-26 13:17:17 +0530

    simple circuit to demonstrate rotation of a qubit
    rotates a qubit in a blochsphere

---
+++ blochsphere.py
@@ -0,0 +1,16 @@
+from qiskit import QuantumCicuit
+
+from qiskit_aer import Aer
+
+qc = QuantumCircuit(1,1)
+
+qc.h(0)
+qc.ry(3.1415/4,0)
+qc.measure(0,0)
+
+backend = Aer.get_backend("aer_simulator")
+job = backend.run(qc, shots=1000)
+
+result = job.result()
+print(result.get_counts())
+print(qc)
